b7ce76e585cc2a3e97fb459887780af4f2baaff7
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-02-22 10:50:33 +0100

    Add ssl options in client.js

--- app/client.js
+++ app/client.js
@@ -12,7 +12,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     idle: 10000, // The maximum time, in milliseconds, that a connection can be idle before being released.
     acquire: 30000 // The maximum time, in milliseconds, that pool will try to get connection before throwing error
   },
-  dialect: "postgres" // The dialect of the database you are connecting to
+  dialect: "postgres", // The dialect of the database you are connecting to
+  dialectOptions: { // An object of additional options, which are passed directly to the connection library
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
