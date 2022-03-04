a3856cebad6d64928634edc1c4d652da19075d59
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-03-04 10:20:57 +0100

    Change status code order into user unit test controller

--- __tests__/controllers/user.test.js
+++ __tests__/controllers/user.test.js
@@ -3,18 +3,6 @@ const Joi = frisby.Joi;
 
 describe('User controller', () => {
   describe('login', () => {
-    it('should return status 400 and an object if mail or password is missing', () => {
-      return frisby
-      .post('https://seb-stan.herokuapp.com/api/v1/login', {
-        email: 'sebastien.parmentier@outlook.fr',
-        password: ''
-      })
-      .expect('status', 400)
-      .expect('json', {
-        data: [],
-        error: "Vous devez entrer un email et un mot de passe"
-      });
-    });
     it('should return status 404 and an object if mail is not correct', () => {
       return frisby
       .post('https://seb-stan.herokuapp.com/api/v1/login', {
@@ -25,6 +13,18 @@ describe('User controller', () => {
       .expect('json', {
         data: [],
         error: "L'email renseigné n'existe pas"
+      });
+    });
+    it('should return status 400 and an object if mail or password is missing', () => {
+      return frisby
+      .post('https://seb-stan.herokuapp.com/api/v1/login', {
+        email: 'sebastien.parmentier@outlook.fr',
+        password: ''
+      })
+      .expect('status', 400)
+      .expect('json', {
+        data: [],
+        error: "Vous devez entrer un email et un mot de passe"
       });
     });
     it('should return status 400 and an object if passord is not correct', () => {
